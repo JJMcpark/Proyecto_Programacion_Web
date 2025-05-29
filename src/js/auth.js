dbd391d147f94834b178d5c94ee3417322abe686
LAB-USR-ATE <LAB-USR-ATE@grupoutp.net>
2025-05-29 10:59:59 -0500

    Actualización y mejora : Primer commit

---
+++ src/js/auth.js
@@ -0,0 +1,16 @@
+
+function openRegisterModal() {
+    document.getElementById('registerModal').style.display = 'flex';
+}
+
+function closeRegisterModal() {
+    document.getElementById('registerModal').style.display = 'none';
+}
+
+function openLoginModal() {
+    document.getElementById('loginModal').style.display = 'flex';
+}
+
+function closeLoginModal() {
+    document.getElementById('loginModal').style.display = 'none';
+}
